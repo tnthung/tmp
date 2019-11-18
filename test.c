d6ae9cc7a3a4be4f5da01c9cbc39b8047b05ad50
tnthung <50406653+tnthung@users.noreply.github.com>
2019-11-18 19:25:28 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void q1() {
+  int odd, even;
+  
+  int i;
+  for (i=0; i<5; i++) {
+    int tmp;
+    scanf("%d", &tmp);
+    if (tmp%2) {
+      even += 1;
+    }
+    else {
+      odd += 1;
+    }
+  }
+  
+  printf("Even: %d\nOdd : %d", even, odd);
+}
 
 int main() {
 
