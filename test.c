00b89bde3269870b44f35247d64d17e5f4d313ed
tnthung <50406653+tnthung@users.noreply.github.com>
2019-11-18 19:29:39 +0800

    Update test.c

--- test.c
+++ test.c
@@ -19,7 +19,36 @@ void q1() {
   printf("Even: %d\nOdd : %d", even, odd);
 }
 
+void q2() {
+  int m;
+  scanf("%d", &m);
+  
+  switch (m) {
+    case 1, 2, 3:
+      printf("Spring");
+      break;
+      
+    case 4, 5, 6:
+      printf("Summer");
+      break;
+      
+    case 7, 8, 9:
+      printf("Fall");
+      break;
+      
+    case 10, 11, 12:
+      printf("Winter");
+      break;
+      
+    default:
+      printf("ERROR");
+  }
+}
+
 int main() {
 
+  //q1();
+  //q2();
+  
   return 0;
 }
