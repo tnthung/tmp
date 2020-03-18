2c8ca07ebe4428e513ec6172321d1e0ce767973b
洪明聖 <hung11112002@gmail.com>
2020-03-18 09:18:15 +0800

    31820-3.cpp

--- 31820-3.cpp
+++ 31820-3.cpp
@@ -2,14 +2,14 @@
 
 
 int main() {
-    int a, b;
+    int a=0, b=0;
     
     std::cin >> a >> b;
     
-    std::cout << a << "+" << b << "=" << a+b;
-    std::cout << a << "-" << b << "=" << a-b;
-    std::cout << a << "*" << b << "=" << a*b;
-    std::cout << a << "/" << b << "=" << a/b << "餘" << a%b;
+    std::cout << a << "+" << b << "=" << a+b << std::endl;
+    std::cout << a << "-" << b << "=" << a-b << std::endl;
+    std::cout << a << "*" << b << "=" << a*b << std::endl;
+    std::cout << a << "/" << b << "=" << a/b << "餘" << a%b << std::endl;
 
     return 0;
 }
