c07f81ff03722ce1582217dd805a2a1b054abf19
洪明聖 <hung11112002@gmail.com>
2020-03-18 18:03:54 +0800

    31820-3.cpp

--- 31820-3.cpp
+++ 31820-3.cpp
@@ -13,6 +13,9 @@ int main() {
         
             if (i==0 || i==a-1 || j=0 || j=b-1)
                 std::cout << "@";
+                
+            else
+                std::cout << " ";
         
         }
         
