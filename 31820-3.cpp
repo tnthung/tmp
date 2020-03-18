1b5c5c5229b291dbcf10bbaa9c3a87ac752c0197
洪明聖 <hung11112002@gmail.com>
2020-03-18 09:43:33 +0800

    31820-3.cpp

--- 31820-3.cpp
+++ 31820-3.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <iomanip>
 
 
 int main() {
     float a;
     
     std::cin >> a;
-    printf("%f\n", a);
+    std::cout << std::setprecision(6) << a << std::endl;
 
     return 0;
 }
