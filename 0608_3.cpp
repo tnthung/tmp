998fcb69606702a4e7fac96e2620019de40433f1
Hung Claier <hung11112002@gmail.com>
2020-06-18 16:54:00 +0800

    asdf

---
+++ 0608_3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main()
+{
+    int a;
+    std::cin >> a;
+    
+    for (int i=0; i<a; i++)
+    {
+        for (int j=0; j<i; j++) std::cout << ' ';
+        for (int j=0; j<i; j++) std::cout << ('A' + j);
+    }
+}
