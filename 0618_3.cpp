df3dac55bb35e22b25fbd4086e156cef4e058ff5
Hung Claier <hung11112002@gmail.com>
2020-06-18 17:06:27 +0800

    asdf

--- 0618_3.cpp
+++ 0618_3.cpp
@@ -9,6 +9,7 @@ int main()
     for (int i=0; i<a; i++)
     {
         for (int j=0; j<i; j++) std::cout << ' ';
-        for (int j=0; j<i; j++) std::cout << ('A' + j);
+        for (int j=0; j<a; j++) std::cout << (char)('A' + j);
+        std::cout << "\n";
     }
 }
